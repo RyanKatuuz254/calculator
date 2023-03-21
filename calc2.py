58a787107946562efc3a4d44aaa3d0c760dd2b1b
Ryan <127747542+RyanKatuuz254@users.noreply.github.com>
2023-03-21 16:22:44 +0300

    Add files via upload

---
+++ calc2.py
@@ -0,0 +1,19 @@
+firstnumber =float(input"enter first number")
+secondnumber =float(input("enter second number"))
+
+myoperator =str(input("Enter operation (+,-,*,/,"))
+
+if myoperator=="+":
+    print(firstnumber+secondnumber)
+elif myoperator=="-":
+    print(firstnumber-secondnumber)
+elif myoperator=="*":
+    print(firstnumber*secondnumber)
+elif myoperator=="/":
+    print(firstnumber/secondnumber)
+else:
+    print("invalid output")
+
+
+
+
